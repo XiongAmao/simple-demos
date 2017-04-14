5e8d1232826250f357607a8b3750c22f91feb4f6
xiongamao <onycat@live.cn>
2017-04-14 17:33:18 +0800

    update style

--- clock-1/clock.js
+++ clock-1/clock.js
@@ -1,36 +1,36 @@
-function step(){
+function step() {
   var now = new Date()
+  
+  var hours = now.getHours()
+  var minutes = now.getMinutes()
+  var seconds = now.getSeconds()
 
-var hours = now.getHours()
-var minutes = now.getMinutes()
-var seconds = now.getSeconds()
-
-if(hours>12){
-  hours -= 12
-}
-
-
-var secondDeg = seconds/60 * 360
-var minuteDeg = (minutes * 60 + seconds) / 3600 * 360
-var hourDeg = (hours*3600 + minutes*60 + seconds) / (12 * 3600) * 360
+  if (hours > 12) {
+    hours -= 12
+  }
 
 
-console.log(secondDeg)
-console.log(minuteDeg)
-console.log(hourDeg)
+  var secondDeg = seconds / 60 * 360
+  var minuteDeg = (minutes * 60 + seconds) / 3600 * 360
+  var hourDeg = (hours * 3600 + minutes * 60 + seconds) / (12 * 3600) * 360
 
-console.log(document.querySelector('.second-hand'))
-document.querySelector('.second-hand')
-  .style.transform = `rotate(${-90+secondDeg}deg)`
 
-document.querySelector('.minute-hand')
-  .style.transform= `rotate(${-90+minuteDeg}deg)`
+  console.log(secondDeg)
+  console.log(minuteDeg)
+  console.log(hourDeg)
 
-document.querySelector('.hour-hand')
-  .style.transform= `rotate(${-90+hourDeg}deg)`
+  console.log(document.querySelector('.second-hand'))
+  document.querySelector('.second-hand')
+    .style.transform = `rotate(${-90 + secondDeg}deg)`
+
+  document.querySelector('.minute-hand')
+    .style.transform = `rotate(${-90 + minuteDeg}deg)`
+
+  document.querySelector('.hour-hand')
+    .style.transform = `rotate(${-90 + hourDeg}deg)`
 }
 
 step()
-setInterval(function(){
+setInterval(function () {
   step()
-},1000)
+}, 1000)
