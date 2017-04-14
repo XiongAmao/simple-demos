1fabfd5052bbb7dd41c6de4184aa6f7f20e1c14a
xiongamao <onycat@live.cn>
2017-04-14 17:39:36 +0800

    update

--- clock-1/clock.js
+++ clock-1/clock.js
@@ -15,11 +15,6 @@ function step() {
   var hourDeg = (hours * 3600 + minutes * 60 + seconds) / (12 * 3600) * 360
 
 
-  console.log(secondDeg)
-  console.log(minuteDeg)
-  console.log(hourDeg)
-
-  console.log(document.querySelector('.second-hand'))
   document.querySelector('.second-hand')
     .style.transform = `rotate(${-90 + secondDeg}deg)`
 
