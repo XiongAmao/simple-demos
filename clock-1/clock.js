6755ab08c17db4a8c711417d22004e28975f81c9
xiongamao <onycat@live.cn>
2017-04-15 23:41:46 +0800

    modify JS

--- clock-1/clock.js
+++ clock-1/clock.js
@@ -5,17 +5,28 @@ function step() {
   var minutes = now.getMinutes()
   var seconds = now.getSeconds()
 
-  if (hours > 12) {
+/*  if (hours > 12) {
     hours -= 12
   }
-
+*/
+  hours %= 12
 
   var secondDeg = seconds / 60 * 360
   var minuteDeg = (minutes * 60 + seconds) / 3600 * 360
   var hourDeg = (hours * 3600 + minutes * 60 + seconds) / (12 * 3600) * 360
+  
 
+  var secondHand = document.querySelector('.second-hand')
+  var minuteHand = document.querySelector('.minute-hand')
+  var hourHand = document.querySelector('.hour-hand')
+  console.log(secondHand)
+  console.log(minuteHand)
+  console.log(hourHand)
+  setHandsStyle(secondDeg,secondHand)
+  setHandsStyle(minuteDeg,minuteHand)
+  setHandsStyle(hourDeg,hourHand)
 
-  document.querySelector('.second-hand')
+/*  document.querySelector('.second-hand')
     .style.transform = `rotate(${-90 + secondDeg}deg)`
 
   document.querySelector('.minute-hand')
@@ -23,7 +34,19 @@ function step() {
 
   document.querySelector('.hour-hand')
     .style.transform = `rotate(${-90 + hourDeg}deg)`
+*/
 }
+function setHandsStyle(deg,hand){
+  if(deg===0){
+    hand.style.transform = `rotate(${deg}deg)`
+    hand.style.transition=`all 0s`
+  }else{
+    hand.style.transform = `rotate(${deg}deg)`
+    hand.style.transition='all 0.3s cubic-bezier(.04, 1.8, .66, .8)'
+  }
+
+}
+
 
 step()
 setInterval(function () {
