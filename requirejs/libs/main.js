d254c70c83f4c71bbbc42ff2e482195ec8a32901
XiongAmao <onycat@live.cn>
2017-06-14 14:18:13 +0800

    update

--- requirejs/libs/main.js
+++ requirejs/libs/main.js
@@ -10,6 +10,6 @@ require.config({
 require(['jquery'], function ($) {
     var $helloworld = $('body').append($('<h1>Hello World!</h1>'))
     setTimeout(() => {
-        $helloworld.append($('<h2>Use jQuery & requireJS!</h2>'))
+        $helloworld.append($('<h2>Hello jQuery & requireJS!</h2>'))
     }, 1000)
 })
